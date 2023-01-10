a4b308c80e2fdf5e805d490fbadd645bae196264
Oscar NM <on5990@gmail.com>
2023-01-09 23:26:19 -0300

    about

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import Header from "../src/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,17 +11,23 @@ export default function Home() {
       <Head>
         <title>Oscar Navarro</title>
       </Head>
-      <header>
-        <nav>
-          <ul>
-            <li>Acerca de mí</li>
-            <li>Proyectos</li>
-            <li>Tecnologías</li>
-            <li>Contáctame</li>
-          </ul>
-        </nav>
-      </header>
-      <main></main>
+      <Header />
+      <main>
+        <div className="about">
+          <h1>Oscar Navarro</h1>
+          <h2>Desarrollador de software</h2>
+          <p>
+            Soy un estudiante universitario de ingeniería informática, con un
+            fuerte interés en el desarrollo web, y estoy motivado para aplicar
+            mis conocimientos y habilidades en proyectos profesionales.
+          </p>
+          <p>
+            Educación: USACH - Ingeniería de Ejecución en Computación e
+            Informática
+          </p>
+          <button>Descargar CV</button>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Acerca de mí</li>
+            <li>Proyectos</li>
+            <li>Tecnologías</li>
+            <li>Contáctame</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
